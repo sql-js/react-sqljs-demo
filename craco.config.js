65915cd2010a313efd88e49829222ff37ea5b38b
calculatingzero <20590197+CalculatingZero@users.noreply.github.com>
2023-12-01 03:24:38 -0800

    update package.json and craco.config.js (#26)
    - added the following dependencies:
    "@babel/plugin-proposal-private-property-in-object": "^7.21.11",
    "browserify-fs": "^1.0.0",
    "crypto-browserify": "^3.12.0",
    "react-scripts": "^5.0.1",
    
    - add fallbacks to the webpackConfig.
    
    - project will now build and start.

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,21 @@
 module.exports = {
     webpack: {
-        configure:{
-            // See https://github.com/webpack/webpack/issues/6725
-            module:{
-                rules: [{
-                    test: /\.wasm$/,
-                    type: 'javascript/auto',
-                }]
-            }
-        }
-    }
-};
+      configure: (webpackConfig) => {
+        // Add the following lines to handle 'crypto' and 'fs' dependencies
+        webpackConfig.resolve.fallback = {
+          fs: require.resolve("browserify-fs"), // or 'empty' if you prefer an empty module
+          crypto: require.resolve('crypto-browserify'),
+          stream: require.resolve('stream-browserify'),
+        };
+  
+        // Add the 'module' configuration for handling .wasm files
+        webpackConfig.module.rules.push({
+          test: /\.wasm$/,
+          type: 'javascript/auto',
+        });
+  
+        return webpackConfig;
+      },
+    },
+  };
+  
